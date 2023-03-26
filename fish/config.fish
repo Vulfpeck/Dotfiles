856b14b704dd9965c9350bf45c081873a9a7a4b5
vulfpeck <tushar.paliwal1997@gmail.com>
2023-03-26 14:15:29 +0530

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+		function fish_user_key_bindings 
+			    bind \cl 'tput reset; clear; commandline -f repaint'
+		end
 end
