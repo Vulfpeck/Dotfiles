6342497672f220a3638a20684477bc5e696ee778
Tushar Paliwal <tushar.paliwal1997@gmail.com>
2023-04-01 16:30:09 +0530

    Update colorscheme.lua

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,8 +1,9 @@
 vim.opt.background = "dark" -- set this to dark or light
+vim.cmd("colorscheme catppuccin-mocha")
 -- vim.cmd.colorscheme("oxocarbon")
-vim.cmd("colorscheme onedark")
+-- vim.cmd("colorscheme onedark")
 -- vim.cmd("colorscheme gruvbox")
-
+-- vim.cmd("colorscheme gruvbox")
 -- vim.cmd "base16colorspace 256"
 -- vim.opt["termguicolors"] = true
 -- vim.cmd "colorscheme base16-tomorrow-night-eighties"
