b44cf3b8d6f377d531e712be9da4ef0ecb3cef3c
vulfpeck <tushar.paliwal1997@gmail.com>
2023-04-08 14:56:45 +0530

    fix: i3 and picom config

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,8 +1,8 @@
 vim.opt.background = "dark" -- set this to dark or light
-vim.cmd("colorscheme github_dark")
+-- vim.cmd("colorscheme github_dark")
 -- vim.cmd("colorscheme catppuccin-latte")
 -- vim.cmd.colorscheme("oxocarbon")
--- vim.cmd("colorscheme onedark")
+vim.cmd("colorscheme onedark_vivid")
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd "base16colorspace 256"
