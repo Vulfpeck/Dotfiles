0c97392ef90902c405cce056eb24d73bd7701e09
vulfpeck <tushar.paliwal1997@gmail.com>
2023-06-12 23:52:03 +0530

    enable gitsigns

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,7 +18,6 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "no"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.api.nvim_set_option("clipboard", "unnamed")
